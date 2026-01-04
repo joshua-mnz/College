ed39269085efd97c912bf82da4192766353b3216
joshuamenezes27 <joshuamnz.2.7@gmail.com>
2026-01-04 10:13:01 +0530

    Updated

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
-defhome():
-return"Hello from Dockerized App!"
+def home():
+    return "Hello from Docker + Flask!"
 
-app.run(host="0.0.0.0", port=8080)
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
